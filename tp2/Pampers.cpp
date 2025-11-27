812e562a3bca728a14cda5e643928b1c37846c6c
Fry <fryadoumorrel@gmail.com>
2025-11-27 09:48:16 +0100

    Pampers finished

---
+++ tp2/Pampers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+class Tableau
+{
+   int * tab;
+   int taille;
+
+ public:
+   Tableau():tab(nullptr), taille(10)
+   {
+       tab = new int[taille]; // si problème ?
+       for(int i = 0;i<taille; i++) {
+        tab[i]=i;
+       }
+   }
+
+   ~Tableau() {
+    delete [] tab;
+   }
+   void afficher() {
+    for(int i = 0;i<taille; i++) {
+        std::cout<<tab[i]<<std::endl;
+       }
+   }
+
+};
+
+int main(int, char **)
+{
+   Tableau t{};
+   t.afficher();
+
+
+   return 0;
+}
