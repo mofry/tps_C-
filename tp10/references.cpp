412d04e70044a25e2d2cc98d55c127d786c038e0
mofry <mofry@a112pc16l.local.isima.fr>
2026-01-13 15:57:39 +0100

    References

---
+++ tp10/references.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+class MemePasPeur {
+   int tab[4];
+ public:
+   MemePasPeur() {
+      tab[0] = 1;
+   }
+   const int & val() const {
+       return tab[0];
+   }
+   void modify() {
+      tab[0] = 4;
+   }
+};
+
+int main(int, char **) {
+  MemePasPeur * p = new MemePasPeur;
+  int i = p->val();
+  const int & j = p->val();
+  std::cout << i << " " << j << std::endl;
+  p->modify();
+  std::cout << i << " " << j << std::endl;
+  delete p;
+  std::cout << i << " " << j << std::endl;  //j "pointe vers une reference 
+  indéfinie// "
+  return 0;
+}
+
+
+
+
+
